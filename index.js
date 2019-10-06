7f1a774c7e07c23d9bbd48a5d9c3cbb7d9592d3f
Avinash verma <avinash-verma-1998>
2019-10-06 17:22:21 +0530

    Cors

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const fileFilter = (req, file, cb) => {
 };
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
     'Access-Control-Allow-Methods',
     'GET, POST, PUT, PATCH, DELETE'
