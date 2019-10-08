97e43513742831f4c53d650ed348150fd6758bf9
Avinash verma <avinash-verma-1998>
2019-10-08 10:10:08 +0530

    majority-writeconcern

--- models/user.js
+++ models/user.js
@@ -1,39 +1,48 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-  email: {
-    type: String,
-    required: true
+const UserSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true
+    },
+
+    name: {
+      type: String,
+      required: true
+    },
+
+    username: {
+      type: String,
+      required: true
+    },
+
+    password: {
+      type: String,
+      required: true
+    },
+
+    createdAt: {
+      type: Date,
+      default: Date.now()
+    },
+
+    resetToken: String,
+
+    resetTokenExpiration: Number,
+
+    followers: [{ type: String }],
+
+    following: [{ type: String }]
   },
+  {
+    writeConcern: {
+      w: 'majority',
+      j: true,
+      wtimeout: 1000
+    }
+  }
+);
 
-  name: {
-    type: String,
-    required: true
-  },
-
-  username: {
-    type: String,
-    required: true
-  },
-
-  password: {
-    type: String,
-    required: true
-  },
-
-  createdAt: {
-    type: Date,
-    default: Date.now()
-  },
-
-  resetToken: String,
-
-  resetTokenExpiration: Number,
-
-  followers: [{ type: String }],
-
-  following: [{ type: String }]
-});
-
-module.exports = User = mongoose.model("user", UserSchema);
+module.exports = User = mongoose.model('user', UserSchema);
