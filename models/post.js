86915794ccd108eaa13ced971ac381100b3384ad
Avinash verma <avinash-verma-1998>
2019-10-08 10:14:11 +0530

    majority-writeconcern-fixed

--- models/post.js
+++ models/post.js
@@ -1,40 +1,49 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const postSchema = new Schema({
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: 'user'
-  },
-  postImageUrl: String,
-  caption: String,
-  comments: [
-    {
-      comment: {
-        type: String
-      },
-      username: String,
-      user: {
-        type: Schema.Types.ObjectId,
-        ref: 'user'
-      },
-
-      commentedOn: {
-        type: Number,
-        default: new Date().getUTCMilliseconds()
-      }
-    }
-  ],
-  likes: [
-    {
+const postSchema = new Schema(
+  {
+    user: {
       type: Schema.Types.ObjectId,
       ref: 'user'
+    },
+    postImageUrl: String,
+    caption: String,
+    comments: [
+      {
+        comment: {
+          type: String
+        },
+        username: String,
+        user: {
+          type: Schema.Types.ObjectId,
+          ref: 'user'
+        },
+
+        commentedOn: {
+          type: Number,
+          default: new Date().getUTCMilliseconds()
+        }
+      }
+    ],
+    likes: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+      }
+    ],
+    createdAt: {
+      type: Date,
+      default: Date.now()
     }
-  ],
-  createdAt: {
-    type: Date,
-    default: Date.now()
+  },
+  {
+    writeConcern: {
+      w: 'majority',
+      j: true,
+      wtimeout: 5000
+    }
   }
-});
+);
 
 module.exports = Post = mongoose.model('post', postSchema);
--- models/profile.js
+++ models/profile.js
@@ -1,16 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const ProfileSchema = new Schema({
-  website: String,
-  bio: String,
-  gender: String,
-  profileImageUrl: String,
+const ProfileSchema = new Schema(
+  {
+    website: String,
+    bio: String,
+    gender: String,
+    profileImageUrl: String,
 
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: 'user'
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'user'
+    }
+  },
+  {
+    writeConcern: {
+      w: 'majority',
+      j: true,
+      wtimeout: 1000
+    }
   }
-});
+);
 
 module.exports = Profile = mongoose.model('profile', ProfileSchema);
